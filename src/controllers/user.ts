6daa06ec3e278de261ae419064e1bf68f1a02b6b
tonkhao <33198781+tonkhao@users.noreply.github.com>
2025-06-26 13:03:53 +0300

    controllers fix

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,9 +4,9 @@ import { Request, Response } from "express";
 export const getUsers = async (_req: Request, res: Response) => {
   try {
     const users = await User.find({});
-    return res.send(users);
+    res.send(users);
   } catch (error) {
-    return res.status(500).send("Ошибка")
+    res.status(500).send({error: "Ошибка получения пользователей"})
   }
 };
 
@@ -14,17 +14,17 @@ export const getUserById = async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
     const user = await User.findById(userId)
-    return res.send(user);
+    res.send(user);
   } catch (error) {
-    return res.status(500).send("Ошибка")
+    res.status(500).send({error: "Ошибка получения пользователя"})
   }
 };
 
 export const createUser = async (req: Request, res: Response) => {
   try {
     const newUser = await User.create(req.body);
-    return res.status(200).send(newUser);
+    res.status(200).send(newUser);
   } catch (error) {
-    return res.status(500).send("Ошибка")
+    res.status(500).send({error: "Ошибка создания пользователя"})
   }
 };
