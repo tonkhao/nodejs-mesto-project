c4a140b529445e8c18813ce9c1da3606c29c1c50
tonkhao <33198781+tonkhao@users.noreply.github.com>
2025-06-30 15:55:01 +0300

    default router added

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import userRouter from './users';
 import cardRouter from './card';
+import { Request, Response } from 'express';
 
 const router = Router();
 
+//Дефолтный раутер для заглушки главной страницы
+router.get('/', (req: Request, res: Response) => {
+  res.send({message: "Добро пожаловать!"})
+})
+
+//основные раутеры
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
